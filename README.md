977c71a9f330401412b986ffb5a9aafda084bc14
Lee <smmang5624@gmail.com>
2024-01-15 18:22:29 +0900

    js을 이용한 top버튼, 슬라이드 기능 완료

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black"/>
 <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=css3&logoColor=white"/>
 2024.01.11 [Mcdonald PC ver.] HEADER, MAIN 작성 완료 <br>
+2024.01.12 [Mcdonald PC ver.] FOOTER 작성 완료 <br>
+2024.01.15 [Mcdonald PC ver.] js을 이용한 슬라이드, top 버튼 작성 완료 <br>
---
+++ js/macdonald.js
@@ -0,0 +1,17 @@
+  // top 버튼 스크롤 기능
+  const topBtn = document.getElementById("top-btn-img");
+  topBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    // console.log(window.scrollY);
+    if (window.scrollY == 0) {
+      window.scrollTo({
+        top: 9999,
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
