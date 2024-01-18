79f370297192906f0597514e7e08aabf2b4c5e41
Lee <smmang5624@gmail.com>
2024-01-18 17:04:34 +0900

    완성 1

--- js/macdonald.js
+++ js/macdonald.js
@@ -1,19 +1,19 @@
 window.onload = function () {
-// top 버튼 스크롤 기능
-const topBtn = document.getElementById("top-btn");
-topBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  // console.log(window.scrollY);
-  if (window.scrollY == 0) {
-    window.scrollTo({
-      top: 9999,
-      behavior: "smooth",
-    });
-  } else {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+  // top 버튼 스크롤 기능
+  const topBtn = document.getElementById("top-btn");
+  topBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    // console.log(window.scrollY);
+    if (window.scrollY == 0) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
   }
-});
-}
