35e123b893ed8eb78d8ae537b28760c88b5f779f
Lee <smmang@gmail.com>
2024-05-13 16:19:49 +0900

    모달창 수정

--- js/macdonald.js
+++ js/macdonald.js
@@ -8,20 +8,44 @@ window.onload = function () {
   //   fadeOut(modal);
   // });
   // ========================================================
-  let body = this.document.querySelector("body");
-  let modal = this.document.querySelector(".modal-wrap");
+  // let body = this.document.querySelector("body");
+  // let modal = this.document.querySelector(".modal-wrap");
+  // modal.addEventListener("click", function () {
+  //   anime({
+  //     targets: ".modal",
+  //     delay: 200,
+  //     duration: 100,
+  //     opacity: 0,
+  //     easing: "easeInOutQuad",
+  //     complete: function () {
+  //       modal.style.display = "none";
+  //       body.classList.add("active");
+  //     },
+  //   });
+  // });
+  // ========================================================
+  // 안내창 스크립트
+  const body = document.querySelector("body");
+  const modal = document.querySelector(".modal-wrap");
   modal.addEventListener("click", function () {
-    anime({
-      targets: ".modal",
-      delay: 200,
-      duration: 100,
-      opacity: 0,
-      easing: "easeInOutQuad",
-      complete: function () {
-        modal.style.display = "none";
-        body.classList.add("active");
-      },
-    });
+    modal.style.display = "none";
+    fadeout(modal);
+  });
+  //isOpen 값에 따라 스크롤을 제어하는 함수
+  function controlScroll(isOpen) {
+    if (isOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "auto";
+    }
+  }
+  // 초기 모달 상태 설정
+  const isOpen = true;
+  controlScroll(isOpen);
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    // 모달이 닫힐 때는 스크롤을 다시 활성화
+    controlScroll(false);
   });
   // ========================================================
   // top 버튼 스크롤 기능
